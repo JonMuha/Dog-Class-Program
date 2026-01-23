79c3afb7cc017e3f4ed20b9928320a657b7c5ab0
Jonathan Muha <jmuha@atu.edu>
2026-01-22 23:23:48 -0600

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,62 @@
+// main.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Dog
+{
+private:
+    string name;
+    int age;
+    string breed;
+
+public:
+    void setName(string);
+    void setAge(int);
+    void setBreed(string);
+    string getName();
+    int getAge();
+    string getBreed();
+};
+
+void Dog::setName(string n)
+{
+
+}
+
+void Dog::setAge(int a)
+{
+
+}
+
+void Dog::setBreed(string b)
+{
+
+}
+
+string Dog::getName()
+{
+
+}
+
+int Dog::getAge()
+{
+
+}
+
+string Dog::getBreed()
+{
+
+}
+
+string bark()
+{
+    cout << "Woof! Woof!";
+}
+
+int main()
+{
+
+}
